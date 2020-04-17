340f7d3aa8c6e762ebbbcc9aef4bb0951c5bd6a3
Adam GM Lewis <adam.lws@gmail.com>
2020-04-17 14:19:27 -0400

    Fix circular dependency

--- drivers.py
+++ drivers.py
@@ -1,6 +1,20 @@
+import numpy as np
+
 import jax_dmrg.operations as ops
 import jax_dmrg.lanczos as lz
 import jax_dmrg.dmrg as dmrg
+
+
+def time_xx(chis=None, N=30, N_sweeps=2):
+    if chis is None:
+        chis = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
+        chis = np.array(chis, dtype=np.int)
+    for chi in chis:
+        print("Timing chi= ", chi)
+        _ = xx_ground_state(N, chi, 1)
+        _, _, _, timings = xx_ground_state(N, chi, N_sweeps)
+        print(timings)
+    return timings
 
 
 def xx_ground_state(N, maxchi, N_sweeps, ncv=20, lz_tol=1E-6, lz_maxiter=10):
